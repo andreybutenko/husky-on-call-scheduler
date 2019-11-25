6a7d0b9114e5fd3fa481aa02591d1d7c26e14a50
Andrey Butenko <ghostmancer@gmail.com>
2019-11-24 20:28:08 -0800

    Add prompts for Andrey's business rules

---
+++ andrey/andrey_business_rules.sql
@@ -0,0 +1,3 @@
+-- Enforce the business rule to prevent employees from taking a shift that they are not qualified for.
+
+-- Enforce the business rule that an employee cannot be assigned to more than one active shift for the same time frame
