6dfa7f1521b6bc16ff340d8cc2b9866ca8f5ce23
Andrey Butenko <ghostmancer@gmail.com>
2019-11-24 20:34:56 -0800

    Revise Andrey's lookup tables

--- andrey/andrey_lookup_tables.sql
+++ andrey/andrey_lookup_tables.sql
@@ -1,19 +1,19 @@
 -- Populate the tblPOSITION_TYPE table
-INSERT INTO tblPOSITION_TYPE (PositionTypeName, PositionTypeDescription)
+INSERT INTO tblPOSITION_TYPE (PositionTypeID, PositionTypeName, PositionTypeDescr)
   VALUES
-    ('RA', 'Resident Advisers and Assistant Resident Directors serve on-call for a building or region'),
-    ('CA', 'Community Assistants serve on-call for grad housing'),
-    ('RD', 'Resident Directors and Community Managers serve on-call for all of campus'),
-    ('AD', 'Administrators and Assistant Directors serve on-call for all of campus'),
-    ('Counselor', 'Counselors serve as a resource for all of campus')
+    (1, 'RA', 'Resident Advisers and Assistant Resident Directors serve on-call for a building or region'),
+    (2, 'CA', 'Community Assistants serve on-call for grad housing'),
+    (3, 'RD', 'Resident Directors and Community Managers serve on-call for all of campus'),
+    (4, 'AD', 'Administrators and Assistant Directors serve on-call for all of campus'),
+    (5, 'Counselor', 'Counselors serve as a resource for all of campus')
 GO
 
 -- Populate the tblGENDER table
-INSERT INTO tblGENDER (GenderTitle, GenderDescription)
+INSERT INTO tblGENDER (GenderID, GenderTitle, GenderDescr)
   VALUES
-    ('Female', 'Female-identifying'),
-    ('Male', 'Male-identifying'),
-    ('Non-binary', 'Non-binary-identifying'),
-    ('Genderfluid', 'Fluid gender identity'),
-    ('Other', 'Other gender identify')
+    (1, 'Female', 'Female-identifying'),
+    (2, 'Male', 'Male-identifying'),
+    (3, 'Non-binary', 'Non-binary-identifying'),
+    (4, 'Genderfluid', 'Fluid gender identity'),
+    (5, 'Other', 'Other gender identify')
 GO
